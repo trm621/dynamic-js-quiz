8dd2412b8198e59f997b5ba06d7be7ced201411d
trm621 <t.maher621@gmail.com>
2021-10-19 15:41:37 -0400

    pseudocoded js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// when user clicks button labelled "begin" the quiz will begin and take them to the first question
+
+// a countdown will begin
+
+// when user clicks one of the options, a sound will let users know if their choice is correct or incorrect
+
+// when users select an incorrect option, time will be deducted from the countdown
+
+// when users answer every question, they will be taken to a page letting them know how they did
+
+// their score will be logged into the localstorage
