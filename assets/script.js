3b0446a457368871739079caf6f8d4406cb28e85
trm621 <t.maher621@gmail.com>
2021-10-19 16:57:07 -0400

    tweaked js so all html content will disappear on beginning button click

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,14 @@ var btn = document.createElement("button")
 var genQuestion = document.createElement("div")
 
 // when user clicks button labelled "begin" the quiz will begin and take them to the first question
-
+var beginQuiz = function() {
     btn.innerHTML = "Click here to begin! Good luck!";
     document.getElementById("quiz-content").appendChild(btn);
-    btn.setAttribute("style", "background-color:black; color:white; padding:50px; font-size:20px; margin:auto; margin-top:100px;");
+    btn.setAttribute("style", "background-color:red; color:white; padding:50px; font-size:20px; margin:auto; margin-top:100px;");
+};
 
     generateQuiz = function() {
-        document.getElementById("quiz-content").removeChild(btn);
+        document.body.innerHTML = "";
     };
 
 
@@ -25,3 +26,5 @@ var genQuestion = document.createElement("div")
 btn.addEventListener("click", function() {
     generateQuiz();    
 });
+
+beginQuiz();
