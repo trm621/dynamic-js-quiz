2cc5d3f22f5fd9c54a1aaa85d970304328cdd486
trm621 <t.maher621@gmail.com>
2021-10-24 23:18:09 -0400

    figured out to use an array, so started building correct code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 // element creators and selectors
 
+const questions = [{question: "Which of the following is NOT a conditional statement.", 
+wrongAnswers:("for", "while", "if"), rightAnswer: "but"},]
+
 var btn = document.createElement("button");
 var h3 = document.createElement("h3");
 var timer = document.getElementById("timer")
@@ -11,12 +14,8 @@ var listItem4 = document.createElement("li");
 var list = document.createElement("ol");
 var divC = document.createElement("div");
 var body = document.body;
-var timeInterval;
-var question1;
-var question2;
-var question3;
-var question4;
-var question5;
+var timeLeft;
+
 
 // when user clicks button labelled "begin" the quiz will begin and take them to the first question
 var beginQuiz = function() {
@@ -31,6 +30,13 @@ var clearLandingPage = function() {
     document.getElementById("wrapper").textContent = "";
 };
 
+var generateQuiz = function() {
+    for (var i=0; i < questions.length; i++) {
+        divC.textContent = questions.question[i];
+        btn.innerhtml = questions.rightAnswer[i] + wrongAnswers[i]
+    }
+}
+
 // a timer will begin countding down from 120 seconds. Ten seconds will be deducted from time when users pick wrong selection
 
 var countdown = function() {
@@ -43,67 +49,19 @@ var countdown = function() {
             document.getElementById("timer").textContent = "";
         } else {
         timer.textContent = timeLeft + " seconds remaining."
-        timeLeft--;
-        }
-        if (question1 === 1) {
-            timeLeft = timeLeft - 10;
-            question1 = question1 + 1;
-        };
-        if (question2 === 1) {
-            timeLeft = timeLeft - 10;
-            question2 = question2 + 1;
-        };
-        if (question3 === 1) {
-            timeLeft = timeLeft - 10;
-            question3 = question3 + 1;
-        };
-        if (question4 === 1) {
-            timeLeft = timeLeft - 10;
-            question4 = question4 + 1;
-        };
-        if (question5 === 1) {
-            timeLeft = timeLeft - 10;
-            question5 = question5 + 1;
+        timeLeft--; 
         };
     }, 1000);
+
+    console.log(timeLeft);
+    return timeLeft;
 };
 
 // question one will display 
 // If the correct choice is selected, the time will remain the same and the next question will display
 // If the incorrect choice is selected, ten seconds will be deducted from the timer
 
-var questionOne = function() {
-    className = "question-one";
-    divC.textContent = "Question One";
-    body.appendChild(divC);
-        divC.setAttribute("style", "font-size:30px; text-align:center;");
-    h3.textContent = "Which of the following is NOT a conditional statement."; 
-        divC.appendChild(h3);
-    divC.appendChild(list);
-    listItem1.textContent = "for";
-        list.appendChild(listItem1);
-        listItem1.addEventListener("click", function () {
-            question1 = 0,
-            questionTwo();
-        });
-    listItem2.textContent = "while";
-        list.appendChild(listItem2);
-        listItem2.addEventListener("click", function() {
-            question1 = 0,
-            questionTwo();
-        });
-    listItem3.textContent = "but";
-        list.appendChild(listItem3);
-        listItem3.addEventListener("click", function() {
-            questionTwo();
-        });
-    listItem4.textContent = "else";
-        list.appendChild(listItem4);
-        listItem4.addEventListener("click", function() {
-            question1 = 0,
-            questionTwo();
-        });
-    };    
+
 
 // question two
 
@@ -177,79 +135,9 @@ var questionThree = function() {
         });
 };
 
-// question four
-
-var questionFour = function() {
-    divC.textContent = "Question Four";
-    body.appendChild(divC);
-        divC.setAttribute("style", "font-size:30px; text-align:center;");
-    h3.textContent = "What does the appendChild function do?"; 
-        divC.appendChild(h3);
-    divC.appendChild(list);
-    listItem1.textContent = "appends a child element to its parent element";
-        list.appendChild(listItem1);
-        listItem1.addEventListener("click", function() {
-            question4 = 0,
-            questionFive();
-        });
-    listItem2.textContent = "creates a child element";
-        list.appendChild(listItem2);
-        listItem2.addEventListener("click", function() {
-            question4 = 1,
-            questionFive();
-        });
-    listItem3.textContent = "deletes a child element";
-        list.appendChild(listItem3);
-        listItem3.addEventListener("click", function() {
-            question4 = 1,
-            questionFive();
-        });
-    listItem4.textContent = "iterates through an array";
-        list.appendChild(listItem4);
-        listItem4.addEventListener("click", function() {
-            question4 = 1,
-            questionFive();
-        });
-};
-
-// question five. When a selection is made, the user will be taken to the endgame page
-
-var questionFive = function() {
-    className = "question-five";
-    divC.textContent = "Question Five";
-    body.appendChild(divC);
-        divC.setAttribute("style", "font-size:30px; text-align:center;");
-    h3.textContent = "When using JavaScript, where should the source file be placed in the HTML?"; 
-        divC.appendChild(h3);
-    divC.appendChild(list);
-    listItem1.textContent = "in the head";
-        list.appendChild(listItem1);
-        listItem1.addEventListener("click", function() {
-            question5 = 1,
-            endScreen();
-        });
-    listItem2.textContent = "at the top of the body";
-        list.appendChild(listItem2);
-        listItem2.addEventListener("click", function() {
-            question5 = 1,
-            endScreen();
-        });
-    listItem3.textContent = "after the closing </html> element";
-        list.appendChild(listItem3);
-        listItem3.addEventListener("click", function() {
-            question5 = 1,
-            endScreen();
-        });
-    listItem4.textContent = "at the end of the body";
-        list.appendChild(listItem4);
-        listItem4.addEventListener("click", function() {
-            question5 = 0,
-            endScreen();
-        });
-    };
-
 var endScreen = function() {
-     
+    displayScore;
+      divC.textContent = "Your score is " + timeLeft + " ."
 };
 
 
@@ -258,7 +146,7 @@ var endScreen = function() {
 btn.addEventListener("click", function() {
     clearLandingPage();
     countdown();
-    questionOne();
+    generateQuiz();
 });
 
 beginQuiz();
